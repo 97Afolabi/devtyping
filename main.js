38b20ebbd8d41613ce24b20a12735ff998b3fee7
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2022-09-07 22:25:51 +0100

    feat: design basic layout

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+const technologies = document.querySelector("#technologies");
+const playground = document.querySelector("#playground");
+const challenge = document.querySelector("#challenge");
+const input = document.querySelector("#touch-typing textarea"); //value of user's input
+const show = document.querySelector("#show");
+const indicator = document.querySelector("#indicator");
+const samplesMenu = document.querySelector("#samples");
+const contributor = document.querySelector("#contributor > a");
+
 async function fetchLanguages() {
+  playground.style.display = "none";
+  input.value = "";
+
   try {
     const response = await fetch("./languages/index.json");
     const data = await response.json();
@@ -10,13 +22,17 @@ async function fetchLanguages() {
 document.addEventListener("load", fetchLanguages());
 
 function renderLanguages(data) {
-  const languages = document.querySelector("#languages");
-
   data.map((language) => {
     const { name, src, description } = language;
     const langBox = document.createElement("div");
-    langBox.innerHTML = `<b>${name}</b><p>${description}</p><button type='submit' onclick='fetchSamples("${src}")'>Take test</button>`;
-    return languages.appendChild(langBox);
+    langBox.className = "technologies";
+    langBox.innerHTML = `
+      <h3 class="technology">${name}</h3>
+      <div class="icon">${name}</div>
+      <p class="description">${description}</p>
+      <button type='submit' class="start" onclick='fetchSamples("${src}")'>Start</button>
+    `;
+    return technologies.appendChild(langBox);
   });
 }
 
@@ -30,43 +46,75 @@ async function fetchSamples(file) {
   }
 }
 
-function renderSample(sample) {
-  var text = (document.querySelector("#text").innerText = sample[3]);
-  var input1 = document.querySelector("#input1"); //value of user's input
-  var show = document.querySelector("#show");
+function renderSample(samples) {
+  technologies.style.display = "none";
+  playground.style.display = "grid";
+  input.style.display = "block";
 
-  input1.addEventListener("keyup", function () {
-    function colourRed() {
-      document.body.style.backgroundColor = "rgba(245,32,32, 0.8)";
-    }
-
-    function colourBlue() {
-      document.body.style.backgroundColor = "rgba(32,32, 245, 0.8)";
-    }
-
-    var inputPos = input1.value.length; //length of user's input
-    if (inputPos == 0) document.body.style.backgroundColor = "#ffffff";
-    //test equality of input and text to be entered
-    input1.value === text.slice(0, inputPos) ? colourBlue() : colourRed();
-    //show.innerHTML = "Incorrectly typed:" +
-    //change the background colour when input is complete
-    text.length == input1.value.length &&
-    input1.value === text.slice(0, inputPos)
-      ? (document.body.style.backgroundColor = "rgba(32, 245,32, 0.8)")
-      : "";
-
-    show.innerHTML =
-      "Characters: " +
-      text.charAt(inputPos - 1) +
-      "&nbsp;" +
-      text.charAt(inputPos) +
-      "&nbsp;" +
-      text.charAt(inputPos + 1) +
-      "&nbsp;" +
-      text.charAt(inputPos + 2) +
-      "&nbsp;" +
-      text.charAt(inputPos + 3) +
-      "&nbsp;" +
-      text.charAt(inputPos + 4);
+  samples.map((sample, index) => {
+    const { title, contributor, text } = sample;
+    const sampleButton = document.createElement("button");
+    sampleButton.type = "button";
+    // sampleButton.id = "sample-" + index;
+    sampleButton.className = "samples";
+    sampleButton.onclick = function () {
+      setChallenge(text);
+      setContributor(contributor);
+    };
+    sampleButton.innerText = title;
+    return samplesMenu.appendChild(sampleButton);
   });
+
+  input.addEventListener("keyup", function () {
+    //   function colourRed() {
+    //     document.body.style.backgroundColor = "rgba(245,32,32, 0.8)";
+    //   }
+
+    //   function colourBlue() {
+    //     document.body.style.backgroundColor = "rgba(32,32, 245, 0.8)";
+    //   }
+
+    let inputPos = input.value.length; //length of user's input
+    //   if (inputPos == 0) document.body.style.backgroundColor = "#ffffff";
+    //   //test equality of input and text to be entered
+    input.value === challenge.innerText.slice(0, inputPos)
+      ? setIndicatorColour("rgba(32,32, 245, 0.8)")
+      : setIndicatorColour("rgba(245,32,32, 0.8)");
+    //   //show.innerHTML = "Incorrectly typed:" +
+    //   //change the background colour when input is complete
+    challenge.innerText.length == input.value.length &&
+    input.value === challenge.innerText.slice(0, inputPos)
+      ? setIndicatorColour("rgba(32, 245,32, 0.8)")
+      : "";
+  });
+}
+
+function setIndicatorColour(colour) {
+  indicator.style.backgroundColor = colour;
+}
+
+function setContributor(name) {
+  contributor.href = "https://github.com/" + name;
+  contributor.innerText = name;
+}
+
+function setChallenge(text) {
+  challenge.innerText = text;
+}
+
+function nextCharacters() {
+  //   show.innerHTML =
+  //     "Characters: " +
+  //     text.charAt(inputPos - 1) +
+  //     "&nbsp;" +
+  //     text.charAt(inputPos) +
+  //     "&nbsp;" +
+  //     text.charAt(inputPos + 1) +
+  //     "&nbsp;" +
+  //     text.charAt(inputPos + 2) +
+  //     "&nbsp;" +
+  //     text.charAt(inputPos + 3) +
+  //     "&nbsp;" +
+  //     text.charAt(inputPos + 4);
+  // });
 }
