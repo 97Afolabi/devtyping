a0030e56c2bd0ce228759a09c4b73a316e30ef29
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2023-10-16 07:58:37 +0100

    update: topic's summary and table of content page

--- src/components/editor/SampleUnselected.tsx
+++ src/components/editor/SampleUnselected.tsx
@@ -1,15 +1,42 @@
-import { SampleUnselectedProp } from "../../lib/interfaces/Editor";
+"use client";
+import { useEffect, useState } from "react";
+import { topic } from "../../lib/data/data";
+import { ExerciseSummary } from "../../lib/data/firebase/exercises";
 import Sidebar from "./Sidebar";
 
-export default function SampleUnselected({
-  data,
-}: {
-  data: SampleUnselectedProp;
-}) {
-  const { samples, slug, title, description } = data;
+export default function SampleUnselected({ slug }: { slug: string }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [samples, setSamples] = useState<ExerciseSummary[]>([]);
+  useEffect(() => {
+    const getSamples = async () => {
+      const data = await topic.get(slug);
+      if (!data) {
+        // TODO: show 404
+      }
+      handleTitleUpdate(data.title);
+      handleDescriptionUpdate(data.description);
+      handleSamplesUpdate(data.samples);
+    };
+
+    getSamples();
+  }, [slug]);
+
+  const handleTitleUpdate = (title: string) => {
+    setTitle(title);
+  };
+
+  const handleDescriptionUpdate = (description: string) => {
+    setDescription(description);
+  };
+
+  const handleSamplesUpdate = (samples: ExerciseSummary[]) => {
+    setSamples(samples);
+  };
+
   return (
     <section className="flex flex-col lg:flex-row w-full h-5/6 py-5 px-2 md:px-10 gap-3">
-      <Sidebar data={samples} slug={slug} contributors={[]}></Sidebar>
+      <Sidebar data={samples} slug={slug} contributors={[]} sampleSelected={false}></Sidebar>
       <div className="basis-4/5 flex flex-col justify-between bg-slate-100 lg:max-2xl:rounded-r-lg max-lg:rounded-b-lg overflow-scroll py-3">
         <section className="w-full font-mono font-semibold">
           <section className="flex h-8 justify-between bg-white py-1 px-4">
