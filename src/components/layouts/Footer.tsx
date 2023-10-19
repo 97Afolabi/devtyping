193a1d5b2c6fd05ac343771bf2581c44bbcafba7
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2023-10-19 12:45:00 +0100

    fix: resolve import conflict

--- src/components/layouts/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="basis-6 bg-slate-900 text-white">
-      <div className="flex justify-center">Footer</div>
-    </footer>
-  );
-}
