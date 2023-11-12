4a89afd506a0a6fae4a0d56496135991530f928f
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2023-11-12 01:20:13 +0100

    update: dimensions of editor pages

--- src/app/review/[...slug]/page.tsx
+++ src/app/review/[...slug]/page.tsx
@@ -9,13 +9,13 @@ const Editor = ({ params }: { params: { slug: string } }) => {
   switch (params.slug.length) {
     case 1:
       return (
-        <section className="flex flex-col lg:flex-row lg:h-full  w-full py-5 px-2 md:px-10 gap-3">
+        <section className="flex flex-col lg:flex-row flex-1 w-full py-5 px-2 md:px-10 gap-3">
           <SampleUnselected prefix="review" slug={topicSlug}></SampleUnselected>
         </section>
       );
     case 2:
       return (
-        <section className="flex flex-col lg:flex-row lg:h-full  w-full py-5 px-2 md:px-10 gap-3">
+        <section className="flex flex-col lg:flex-row flex-1 w-full py-5 px-2 md:px-10 gap-3">
           <SampleSelected
             prefix="review"
             topicSlug={topicSlug}
